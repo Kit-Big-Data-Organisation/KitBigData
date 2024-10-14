d816e3b05acb7444d18c31b4f3aafac3f5988f9a
Ghalia Chaoui <ghalia.chaoui@gmail.com>
2024-10-14 17:52:11 +0200

    Correction du format de main pour test du workflow ci.yaml

--- projet_kbd/main.py
+++ projet_kbd/main.py
@@ -1,6 +1,18 @@
+"""
+This module contains the main entry point for the project.
+It prints a message indicating that the project is running successfully.
+"""
 # main.py
 
+
 def main():
+    """
+    Main function that prints a success message.
+
+    This function serves as the entry point of the program and
+    outputs a message indicating that the project is running successfully.
+    """
+
     print("Project is running successfully!")
 
 
