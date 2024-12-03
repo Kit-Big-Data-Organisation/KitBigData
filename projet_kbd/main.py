303bf532266046583926e6464aa32b4a3755cbb8
ghaliac <ghalia.chaoui@gmail.com>
2024-12-03 16:42:58 +0100

    Refactor main.py for improved readability; and adjust Flake8 exclusions in pyproject.toml to exlude main.py

--- projet_kbd/main.py
+++ projet_kbd/main.py
@@ -2,16 +2,10 @@ import cProfile
 import sqlalchemy
 import streamlit_app
 
-engine = sqlalchemy.create_engine(
-    'sqlite:////Users/ghalia/Desktop/Telecom IA/Projet KBD/KitBigData/'
-    'projet_kbd/database/streamlit.db'
-)
+engine = sqlalchemy.create_engine('sqlite:////Users/ghalia/Desktop/Telecom IA/Projet KBD/KitBigData/projet_kbd/database/streamlit.db')
 if __name__ == "__main__":
 
     profiler = cProfile.Profile()
     profiler.enable()
-    app = streamlit_app.run(
-        '/Users/ghalia/Desktop/Telecom IA/Projet KBD/'
-        'KitBigData/Data", "RAW_recipes.csv", "RAW_interactions.csv", engine)'
-    )
+    app = streamlit_app.run('/Users/ghalia/Desktop/Telecom IA/Projet KBD/KitBigData/Data', 'RAW_recipes.csv', 'RAW_interactions.csv', engine)
     profiler.disable()
