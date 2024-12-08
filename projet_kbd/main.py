6be0a52bc9fdc7b7239e16faabad4132c8a4d0d8
ghaliac <ghalia.chaoui@gmail.com>
2024-12-08 18:49:41 +0100

    Update streamlit.db URL to new location

--- projet_kbd/main.py
+++ projet_kbd/main.py
@@ -25,7 +25,7 @@ INTERACTIONS_FILE = "RAW_interactions.csv"
 DATA_FILES = {
     "RAW_recipes.csv": ("https://drive.google.com/uc?id=1mrct6Jo7PjwHnFpNZ3JKc2g1dvGaeC0w", DATA_DIR),
     "RAW_interactions.csv": ("https://drive.google.com/uc?id=1ikGYx3h741P2JIkFwIclhciiIGeBWpTL", DATA_DIR),
-    "streamlit.db": ("https://drive.google.com/uc?id=1C089STCZ8A9qREtGJE9X-YJ9dfZbiHhV", os.path.join(BASE_DIR, "projet_kbd", "database"))
+    "streamlit.db": ("https://drive.google.com/uc?id=1LboRS888bE4EaKGRQosd_4LspGFOADs7", os.path.join(BASE_DIR, "projet_kbd", "database"))
 }
 
 # Create SQLAlchemy engine dynamically
